1475f657eafc93c50e65d5850b5fdeb31c193b2a
Amber Ostendorf <ambersed_2000@yahoo.com>
2018-05-16 09:23:12 -0600

    update on comchoice

--- main.js
+++ main.js
@@ -4,17 +4,17 @@ $(document).ready( function() {
   /*var choice = 0
   var selection = $('.entry')*/
 
-  
+
   $(".dropdown").on('change', function() {
     var player = $(this).prop("value")
     console.log(player)
     $('#selection').html("You have selected: " + player)
   })
 
-  $(".play").on('click', function() {
-    var elements = ["Rock", "Paper", "Scissors"]
-    var computer = elements[Math.floor(Math.random() * elements.length)]
-    console.log(computer)
-    $('computerPick').html("The computer chose: " + computer)    
+  $("#play").on('click', function() {
+    var options = ['Rock', 'Paper', 'Scissors']
+    var comChoice = Math.floor(Math.random() * options.length)
+    }
+    
   })
 })
